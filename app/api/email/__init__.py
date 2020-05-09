a99e18f3b0c1627e9f032ade8d1b62d6c697b9e9
Debdut Goswami <debdutgoswami@gmail.com>
2020-05-09 23:46:56 +0530

    AUTH

---
+++ app/api/email/__init__.py
@@ -0,0 +1,1 @@
+# app/api/email/__init__.py
---
+++ app/api/email/tasks.py
@@ -0,0 +1,29 @@
+import os
+from flask import render_template
+from flask_mail import Message
+from api import app, mail, celery
+
+@celery.task
+def deliver_contact_email(subject, name, email, link):
+    """
+    Send a contact e-mail.
+    :param email: E-mail address of the visitor
+    :type user_id: str
+    :param message: E-mail message
+    :type user_id: str
+    :return: None
+    """
+    msg = Message(
+        subject=subject,
+        sender=app.config['MAIL_DEFAULT_SENDER'],
+        recipients=[email]
+    )
+
+    try:
+        with app.app_context():
+            msg.html = render_template('confirmation.html', link=link, name=name)
+            mail.send(msg)
+    except TimeoutError:
+        with app.app_context():
+            msg.html = render_template('confirmation.html', link=link, name=name)
+            mail.send(msg)
