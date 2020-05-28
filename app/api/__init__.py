dbe3a89a0b31708b8cd770a0337b5521a7e2c586
Debdut Goswami <debdutgoswami@gmail.com>
2020-05-29 02:44:28 +0530

    FIX celery
    Fixed the error api.celery modulenotfound error

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -20,8 +20,8 @@ CELERY_TASK_LIST = [
     'api.celery'
 ]
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-#celery.conf.update(app.config)
+celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 from api.routes import api
 
