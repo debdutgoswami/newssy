c45b015cdcf6cc17f83905f20fa7a8033b53f481
Debdut Goswami <debdutgoswami@gmail.com>
2020-05-30 18:35:41 +0530

    Add functionality  to Profile Routes
    addded the functionality of necessary routes for profile management. Also added added VERIFIED user attribute for db.

--- app/api/models/users.py
+++ app/api/models/users.py
@@ -13,24 +13,29 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     joined_on =db.Column(db.String)
+    name_changed_on = db.Column(db.String)
     admin = db.Column(db.Boolean)
     preferences = db.Column(ARRAY(db.String))
     saved_article = db.Column(ARRAY(db.String))
     email_notify = db.Column(db.Boolean)
-    whatsapp_notify = db.Column(db.Boolean)
     confirmed = db.Column(db.Boolean)
+    VERIFIED = db.Column(db.Boolean) # verified tick like Twitter
 
-    def __init__(self, name, email, password, preferences=None, admin=False, email_notify=False, whatsapp_notify=False, confirmed=False):
+    def __init__(self, name, email, password, preferences=None, saved_article=None, email_notify=False, confirmed=False):
         self.public_id = uuid.uuid4()
         self.name = name
         self.email = email
         self.password = bcrypt.generate_password_hash(
             password, app.config.get('BCRYPT_LOG_ROUNDS')
         ).decode()
+        self.admin = False
         self.joined_on = datetime.datetime.utcnow()
         self.email_notify = email_notify
-        self.whatsapp_notify = whatsapp_notify
+        self.name_changed_on = self.joined_on
         self.confirmed = confirmed
+        self.preferences = preferences
+        self.saved_article = saved_article
+        self.VERIFIED = False
 
     def __repr__(self):
         return f"<User(name={self.name}, admin={self.admin})>"
