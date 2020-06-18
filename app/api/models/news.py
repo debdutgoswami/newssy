f646beb7b2ecf807d8c001dc4bccbb1afd9354a9
Debdut Goswami <debdutgoswami@gmail.com>
2020-06-19 03:37:55 +0530

    Changed database schema

--- app/api/models/news.py
+++ app/api/models/news.py
@@ -10,9 +10,9 @@ class News(db.Model):
     public_id = db.Column(db.String, unique=True, nullable=False) # refering to url safe and unique
     # obtained by scraping
     country = db.Column(db.String, nullable=False)
-    title = db.Column(BYTEA, unique=True, nullable=False)
+    title = db.Column(db.String, unique=True, nullable=False)
     url = db.Column(db.String, nullable=False, unique=True)
-    body = db.Column(BYTEA, nullable=False)
+
     source = db.Column(db.String, nullable=False) # name of the news vendor
     lastupdated = db.Column(db.String, nullable=False) # time the news was added
     # obtained from Ml model
