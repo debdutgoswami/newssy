84889f145e93d13d03458e8f175728c3a5f3c2d5
Debdut Goswami <debdutgoswami@gmail.com>
2020-06-30 02:47:27 +0530

    fix cloudsqlproxy not working on compute instance

--- src/backend/api/__init__.py
+++ src/backend/api/__init__.py
@@ -1,5 +1,3 @@
-from flask_cors import CORS
-
 from flask import Flask, Blueprint, request
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
@@ -12,8 +10,6 @@ from celery import Celery
 import os
 
 app = Flask(__name__, static_folder='../build', static_url_path='/',template_folder=os.path.join(os.getcwd(),'api','email','templates'))
-
-CORS(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,8 +28,3 @@ celery.conf.update(app.config)
 from api.routes import api
 
 app.register_blueprint(api, url_prefix='/api')
-
-# @app.route('/')
-# def index():
-#     # serves index.html generated from REACT
-#     return app.send_static_file('index.html')
