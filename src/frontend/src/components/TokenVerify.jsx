42a1aca48f45a3e38c46af3c4b2fa5b450ae6daf
skypaul2000@gmail.com <skypaul2000@gmail.com>
2020-07-06 22:08:48 +0530

    Token

--- src/frontend/src/components/TokenVerify.jsx
+++ src/frontend/src/components/TokenVerify.jsx
@@ -9,11 +9,13 @@ class Token extends Component {
     const q = window.location.search;
     const url = new URLSearchParams(q);
     const token = url.get("token");
-    const apiEndpoint = apiUrl + "/confirm/" + token;
-
-    const response = await axios.get(apiEndpoint).catch((err) => {
+    const apiEndpoint = apiUrl + "/confirm";
+    const response = await axios.post(apiEndpoint, { token }).catch((err) => {
       this.setState({ status: err.response.status });
     });
+    // const response = await axios.post(apiEndpoint).catch((err) => {
+    //   this.setState({ status: err.response.status });
+    // });
 
     try {
       const status = response.status;
